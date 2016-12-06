8d0ad56d2682c9e09d2148ea883b8bccfddc8680
alma18-meet <none>
2016-12-06 13:49:04 -0500

    Fixed import

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -1,8 +1,8 @@
 #This script performs some simple tests on the UserAccount class.
-import UserAccount
+from UserAccount import UserAccount
 
 #Three things are missing from the line below - fill them in
-my_user=UserAcount("alma18-meet","meetyear18","smile")
+my_user=UserAccount("alma18-meet","meetyear18","smile")
 
 #Call the print_secret method (function) - it takes one input - a guess for the password.
 my_user.print_secret("meetyear18")
