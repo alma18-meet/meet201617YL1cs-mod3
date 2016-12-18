f02ebcaa3b8423e526d16b599b6e76dfc081f0a5
none <none>
2016-12-18 19:04:11 +0200

    made tests for UserAccount class

--- TestUserAccount.py
+++ TestUserAccount.py
@@ -5,9 +5,12 @@ from UserAccount import UserAccount
 my_user=UserAccount("alma18-meet","meetyear18","smile")
 
 #Call the print_secret method (function) - it takes one input - a guess for the password.
-my_user.print_secret("meetyear18")
+my_user.print_secret("meeetyear18")
 #Use the wrong password as input here
-my_user.
+#my_user.password("meeetyear18")
 
 #Use the right password here
-my_user.
+#my_user.password("meetyear18")
+
+
+my_user.print_secret("meetyear18")
